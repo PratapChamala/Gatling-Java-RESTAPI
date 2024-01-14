87673a1b3b8144c768fc71014f2a3969f9ecf12c
Pratap Chamala <prathapchamala@gmail.com>
2024-01-13 21:13:45 -0500

    Initial Gatling java project

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
+<<<<<<< HEAD
 # Gatling-java-sample
 Load test REST API Using Gatling and Java 
+=======
+<<<<<<< HEAD
+# Gatling-java-sample
+Load test REST API Using Gatling and Java 
+=======
+Loadtest REST API using Gatling - Java 
+===================================================
+
+#### ==> Do not execute with more than one user and only one execution
+#### ==> All the requests are made to reqres.in website 
+##### ==> This website is not designed for load testing
+#### ===> Enable one request at a time
+##### This project is an extension UI application using gatling https://github.com/gatling/gatling-maven-plugin-demo-java
+
+#### Scenarios Covered
+    1.  How to get authtoken using jsonpath and pass it in another request headers
+    2.  Call Get endpoint,capture response and assert 
+    3.  Write response to console to see response
+    4.  Capture response data into array and exeute until array is empty
+    5.  Call child thread /depend
+    6.  Post request - Boday as String
+    7.  Loops - forever , repeat , doWhile ..
+    8.  Multiple endpoints in one scenario, using chainbuilder
+    9.  Call authtoken endpoint once and repete other endpoints forever
+    10. InjectOpen and InjectClose -difference
+    11. If Assertion fails exit the loop 
+    12. Assign users for each endpoint as needded 
+    13. Print session data , using session data
+
+#### Feeders 
+    14. how to read data from files for post and get calls
+    15. how to feed dynamic values from csv file
+    16. how to create dynamic data for each request using custome feeders
+            i.e using date, random number , UUID , names ..etc
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> 4241804 (Initial Gatling Java testing)
+>>>>>>> 5fb1669 (first commit)
---
+++ src/test/java/restservices/Restsample.java
@@ -0,0 +1,71 @@
+package restservices;
+
+import io.gatling.javaapi.core.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class Restsample extends Simulation {
+    private final HttpProtocolBuilder protocol = http.baseUrl("https://reqres.in/api/");
+
+    // #1 #2 and #3 Get AuthToken
+    private final ChainBuilder getAuthToken = exec(http("T01-GetAuthToken")
+            .post("/register")
+            .header("content-type", "application/json")
+            .body(StringBody("{\n" +
+                    "    \"email\": \"eve.holt@reqres.in\",\n" +
+                    "    \"password\": \"pistol\"\n" +
+                    "}"))
+            // assert for 200 status
+            .check(status().is(200))
+            // capture token using jsonpath and saving into a variable
+            .check(jsonPath("$.token")
+                    .saveAs("AUTH_TOKEN"))).exitHereIfFailed() // #11 exit here if fail
+            // #13 Writer response to console and session data
+            .exec(session -> {
+                System.out.println("Response Body:");
+                System.out.println(session.getString("AUTH_TOKEN"));
+                return session;
+            });
+    // #4 and # 5 pause(1) , pause(100,200)
+    private final ChainBuilder getAllUsers = exec(http("T02-GetAllUsers").get("/users?page=2")
+            .header("content-type", "application/json")
+            .check(jsonPath("$.data[*].id").findAll().saveAs("users")))
+            .foreach("#{users}", "user") // foreach loop through users list
+            .on(exec(http("T02.1-GetSingleUser")
+                    .get("https://reqres.in/api/users/#{user}")
+                    .check(status().is(200))).pause(1));
+    private final ChainBuilder createUser = exec(http("T03-CreateUser")
+            .post("/api/users")
+            .body(StringBody("{\n" +
+                    "    \"name\": \"morpheus\",\n" +
+                    "    \"job\": \"leader\"\n" +
+                    "}")).asJson()
+            .header("content-type", "application/json")
+            .check(status().is(201)));
+
+    private final ScenarioBuilder authToken = scenario("GetAuthToken").exec(getAuthToken); // #1 Get auth_token request
+    private final ScenarioBuilder allUsers = scenario("getAllUsers").exec(getAllUsers); // // #4 and # 5
+    private final ScenarioBuilder createusers = scenario("CreateUser")
+            .repeat(3).on(exec(createUser).pause(1)); // #6 #7 repeat
+    private final ScenarioBuilder allendpoints = scenario("AllEndpointsSequential")
+            .exec(getAuthToken.pace(1), createUser.pace(1), getAllUsers.pace(1)); // #8 all endpoints at once
+    private final ScenarioBuilder scn = scenario("CreateUser")
+            .exec(getAuthToken)
+            .forever().on(pace(1).exec(createUser)); // #9 Execute token endpoint once and create user forever
+    {
+
+         setUp(authToken.injectOpen(atOnceUsers(1)).protocols(protocol)).maxDuration(1); // #1 Get auth_token request
+        // setUp(allUsers.injectOpen(atOnceUsers(1))).protocols(protocol).maxDuration(1); // #2 Get all users request
+        // setUp(createusers.injectOpen(atOnceUsers(1))).protocols(protocol).maxDuration(1); // #6 #7 repeat
+        // setUp(allendpoints.injectOpen(atOnceUsers(1))).protocols(protocol).maxDuration(10); // #8 all endpoints at once
+        // setUp(scn.injectOpen(atOnceUsers(1))).protocols(protocol).maxDuration(10); // #9
+        // setUp(authToken.injectOpen(atOnceUsers(1)),allUsers.injectOpen(atOnceUsers(2)),
+        //         createusers.injectOpen(atOnceUsers(3)).protocols(protocol)).maxDuration(15); // #12 assign threads for each endpoint
+
+    }
+
+}
